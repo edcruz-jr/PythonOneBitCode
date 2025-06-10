f4f48c981786c48e496ae3b2342d1dbfd4279d58
edcruz-jr <edcruz_jr@outlook.com>
2025-06-09 22:20:13 -0300

    :construction: Trabalhando com operadores

---
+++ 5-operadores.py
@@ -0,0 +1,27 @@
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+
+# Aritméticos
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+print(sum)
+print(sub)
+
+# Comparação
+bigger = num1 > num2
+smaller = num1 < num2   
+equal = num1 == num2
+different = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+# Atribuição
+num1 = num1 + 1
+num1 += 1
+num1 -= 1
+num1 *= 1
+num1 /= 1
