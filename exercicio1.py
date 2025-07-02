1d7b6ef4eb31de12cb20d60554bf08fe09a2826f
edcruz-jr <edcruz_jr@outlook.com>
2025-07-02 18:44:59 -0300

    :construction: Adicionando o enunciado e modificando as variáveis

--- exercicio1.py
+++ exercicio1.py
@@ -1,13 +1,15 @@
+# Exercício 1 - Antecessor e Sucessor
 num1 = float(input("Digite o primeiro número: "))
 antecessor = num1 - 1
 sucessor = num1 + 1
 
 print(f"O número digitado foi {num1}, o seu antecessor é {antecessor} e seu sucessor é {sucessor}")
 
-num2 = float(input("Digite o segundo número: "))
-num3 = float(input("Digite o terceciro número: "))
-num4 = float(input("Digite o quarto número: "))
+note1 = float(input("Digite o primeiro número: "))
+note2 = float(input("Digite o segundo número: "))
+note3 = float(input("Digite o terceciro número: "))
+note4 = float(input("Digite o quarto número: "))
 
-media = (num1 + num2 + num3 + num4) / 4
+media = (note1 + note2 + note3 + note4) / 4
 
-print(f"O números digitados foram {num1}, {num2}, {num3}, {num4} e a média entre eles é {media}")
+print(f"O números digitados foram {note1}, {note2}, {note3}, {note4} e a média entre eles é {media}")
