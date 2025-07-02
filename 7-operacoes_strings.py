ded8c4c4045d2cc4817dd030d4374ec7956179ff
edcruz-jr <edcruz_jr@outlook.com>
2025-07-02 19:04:57 -0300

    :construction: Operações com strings

---
+++ 7-operacoes_strings.py
@@ -0,0 +1,19 @@
+gameDescription = """
+Fifa 23 é um jogo de futebol Desenvolvido pela EA Sports
+É possível jogar online
+"""
+
+gameName = "Fifa"
+gameVersion = "23"
+
+# 1 - Operação de concatenação de Strings
+gameFullName = gameName + " " + gameVersion
+print(gameFullName)
+
+# 2 - Operação de multiplicação de Strings
+line = "="
+print(line * 10)
+
+# 3 - Procurar uma palavra dentro de um texto
+print("Fifa" in gameDescription)
+print("futebol" in gameDescription)
