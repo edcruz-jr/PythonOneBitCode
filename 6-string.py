87ede53ee4d506dc76865888475389d35555a5b7
edcruz-jr <edcruz_jr@outlook.com>
2025-07-02 18:57:13 -0300

    construction: Trabalhando com strings

---
+++ 6-string.py
@@ -0,0 +1,13 @@
+gameName1 = "Fifa 23"
+gameName2 = "fifa 23"
+
+print(gameName1)
+print(gameName2)
+print(gameName1 == gameName2) #Verificando se os dois textos são iguais para verificação de Case Sensitive
+
+gameDescription = """
+    Fifa 23 é um jogo de futebol
+    Desenvolvido pela EA Sports
+    É possível jogar online
+"""
+print(gameDescription)
